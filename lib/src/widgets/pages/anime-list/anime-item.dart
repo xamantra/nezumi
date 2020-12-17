f2dd517f1c2fe6a73ece41a01310234e6f99d8ee
xamantra <xamantrx@gmail.com>
2020-12-17 20:49:50 +0800

    slidable anime card item for edit

--- lib/src/widgets/pages/anime-list/anime-item.dart
+++ lib/src/widgets/pages/anime-list/anime-item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:relative_scale/relative_scale.dart';
 
 import '../../../data/index.dart';
@@ -14,114 +15,138 @@ class AnimeItemCard extends StatelessWidget {
     var color = AppTheme.of(context).secondaryBackground.withOpacity(0.6);
     return RelativeBuilder(
       builder: (context, height, width, sy, sx) {
-        return Container(
-          width: width,
-          margin: EdgeInsets.all(sy(4)),
-          decoration: BoxDecoration(
-            color: color,
-            borderRadius: BorderRadius.circular(5),
-            boxShadow: [
-              BoxShadow(
-                offset: Offset(0, 0.5),
-                spreadRadius: 1,
-                blurRadius: 1,
-                color: Colors.black.withOpacity(0.1),
-              ),
-            ],
-          ),
-          child: Ripple(
-            padding: sy(8),
-            onPressed: () {},
-            child: Row(
-              children: [
-                SymmetricImage(
-                  url: anime?.node?.mainPicture?.medium ?? '',
-                  size: sy(36),
+        var actions = [
+          Container(
+            padding: EdgeInsets.symmetric(vertical: sy(4)),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(5),
+              child: IconSlideAction(
+                color: Colors.transparent,
+                iconWidget: Icon(
+                  Icons.edit,
+                  size: sy(20),
+                  color: Colors.blue,
                 ),
-                SizedBox(width: sy(8)),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        anime.node.title,
-                        style: TextStyle(
-                          color: AppTheme.of(context).text3,
-                          fontWeight: FontWeight.w600,
-                          fontSize: sy(10),
-                        ),
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      SizedBox(height: sy(4)),
-                      Row(
-                        children: [
-                          Text(
-                            anime.node.mediaType.toUpperCase(),
-                            style: TextStyle(
-                              color: AppTheme.of(context).text3,
-                              fontWeight: FontWeight.w300,
-                              fontSize: sy(7),
-                            ),
-                          ),
-                          Dot(color: AppTheme.of(context).text3),
-                          Text(
-                            anime.animeStatus,
-                            style: TextStyle(
-                              color: AppTheme.of(context).text3,
-                              fontWeight: FontWeight.w300,
-                              fontSize: sy(7),
-                            ),
-                          ),
-                          Dot(color: AppTheme.of(context).text3),
-                          Text(
-                            '${anime.durationPerEpisode} mins x ${anime.episodeCount}',
-                            style: TextStyle(
-                              color: AppTheme.of(context).text3,
-                              fontWeight: FontWeight.w300,
-                              fontSize: sy(7),
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: sy(4)),
-                      SingleChildScrollView(
-                        scrollDirection: Axis.horizontal,
-                        child: Row(
-                          children: [
-                            Badge(
-                              color: Colors.teal,
-                              textColor: Colors.white,
-                              text: anime.season,
-                              fontSize: sy(6),
-                            ),
-                            SizedBox(width: sy(4)),
-                            Badge(
-                              color: Colors.redAccent,
-                              textColor: Colors.white,
-                              text: anime.source,
-                              fontSize: sy(6),
-                            ),
-                          ]..addAll(anime.studios.map(
-                              (e) => Row(
-                                children: [
-                                  SizedBox(width: sy(4)),
-                                  Badge(
-                                    color: Colors.purple,
-                                    textColor: Colors.white,
-                                    text: e,
-                                    fontSize: sy(6),
-                                  ),
-                                ],
-                              ),
-                            )),
-                        ),
-                      ),
-                    ],
-                  ),
+                onTap: () {},
+                closeOnTap: false,
+              ),
+            ),
+          ),
+        ];
+        return Slidable(
+          actionPane: SlidableStrechActionPane(),
+          actionExtentRatio: 0.25,
+          child: Container(
+            width: width,
+            margin: EdgeInsets.all(sy(4)),
+            decoration: BoxDecoration(
+              color: color,
+              borderRadius: BorderRadius.circular(5),
+              boxShadow: [
+                BoxShadow(
+                  offset: Offset(0, 0.5),
+                  spreadRadius: 1,
+                  blurRadius: 1,
+                  color: Colors.black.withOpacity(0.1),
                 ),
               ],
             ),
+            child: Ripple(
+              padding: sy(8),
+              onPressed: () {},
+              child: Row(
+                children: [
+                  SymmetricImage(
+                    url: anime?.node?.mainPicture?.medium ?? '',
+                    size: sy(36),
+                  ),
+                  SizedBox(width: sy(8)),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          anime.node.title,
+                          style: TextStyle(
+                            color: AppTheme.of(context).text3,
+                            fontWeight: FontWeight.w600,
+                            fontSize: sy(10),
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        SizedBox(height: sy(4)),
+                        Row(
+                          children: [
+                            Text(
+                              anime.node.mediaType.toUpperCase(),
+                              style: TextStyle(
+                                color: AppTheme.of(context).text3,
+                                fontWeight: FontWeight.w300,
+                                fontSize: sy(7),
+                              ),
+                            ),
+                            Dot(color: AppTheme.of(context).text3),
+                            Text(
+                              anime.animeStatus,
+                              style: TextStyle(
+                                color: AppTheme.of(context).text3,
+                                fontWeight: FontWeight.w300,
+                                fontSize: sy(7),
+                              ),
+                            ),
+                            Dot(color: AppTheme.of(context).text3),
+                            Text(
+                              '${anime.durationPerEpisode} mins x ${anime.episodeCount}',
+                              style: TextStyle(
+                                color: AppTheme.of(context).text3,
+                                fontWeight: FontWeight.w300,
+                                fontSize: sy(7),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: sy(4)),
+                        SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Row(
+                            children: [
+                              Badge(
+                                color: Colors.teal,
+                                textColor: Colors.white,
+                                text: anime.season,
+                                fontSize: sy(6),
+                              ),
+                              SizedBox(width: sy(4)),
+                              Badge(
+                                color: Colors.redAccent,
+                                textColor: Colors.white,
+                                text: anime.source,
+                                fontSize: sy(6),
+                              ),
+                            ]..addAll(anime.studios.map(
+                                (e) => Row(
+                                  children: [
+                                    SizedBox(width: sy(4)),
+                                    Badge(
+                                      color: Colors.purple,
+                                      textColor: Colors.white,
+                                      text: e,
+                                      fontSize: sy(6),
+                                    ),
+                                  ],
+                                ),
+                              )),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
+          actions: actions,
+          secondaryActions: actions,
         );
       },
     );
