7ae01eb515fa325c95d592cd4e553bad49ab13bb
xamantra <xamantrx@gmail.com>
2021-01-29 21:51:45 +0800

    TODO items

--- lib/src/todo.dart
+++ lib/src/todo.dart
@@ -2,8 +2,13 @@
 // TODO: Yearly Anime Rankings -> implement genre, list status, and airing status filters.
 // TODO: decimal scoring system.
 // TODO: delete anime from list.
-// TODO: full unit testing before moving on to manga pages.
 // TODO: export list
+// TODO: details/help prompt for `StatBox`
+// TODO: Anime filter date format (MMM d, y).
+// TODO: Anime filter personal score filter.
+// TODO: Anime filter global score filter.
+// TODO: Anime profile page.
+// TODO: full unit testing before moving on to manga pages.
 
 // vs code doesn't highlight todo items in an empty file. so had to put this.
 void main() {}
